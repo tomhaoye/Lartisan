341d9805d7b35fe36473955ebcc0e291aee6fe2f
邱奕玮 <tomhaoye@163.com>
2016-03-30 00:52:16 +0800

    migrate

--- app/Socket/ws_server.php
+++ app/Socket/ws_server.php
@@ -6,12 +6,13 @@ $ws = new swoole_websocket_server("0.0.0.0", 9502);
 //监听WebSocket连接打开事件
 $ws->on('open', function ($ws, $request) {
     var_dump($request->fd, $request->server);
+
     $ws->push($request->fd, "hello, welcome\n");
 });
 
 //监听WebSocket消息事件
 $ws->on('message', function ($ws, $frame) {
-    echo "Message: {$frame->data}\n";
+
     $ws->push($frame->fd, "{$frame->fd}said: {$frame->data}");
 });
 
