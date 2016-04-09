f4a0b53aea7d49e9a556c11547a760d4544cdb77
邱奕玮 <tomhaoye@163.com>
2016-04-09 14:40:43 +0800

    eliminate v_d

--- app/Socket/ws_server.php
+++ app/Socket/ws_server.php
@@ -5,8 +5,6 @@ $ws = new swoole_websocket_server("0.0.0.0", 9502);
 
 //监听WebSocket连接打开事件
 $ws->on('open', function ($ws, $request) {
-    var_dump($request->fd, $request->server);
-
     $ws->push($request->fd, "hello, welcome\n");
 });
 
