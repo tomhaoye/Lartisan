8e2c50ecd28ed4f5f55ef292ac4abc4edbf3a6c8
邱奕玮 <tomhaoye@163.com>
2016-04-17 15:30:00 +0800

    model rule

--- app/Models/Topics.php
+++ app/Models/Topics.php
@@ -12,6 +12,15 @@ class Topics extends Model
     public $primaryKey = 'id';
     protected $guarded = array('id');
 
+    public static $createJobRule = array(
+        'title' => 'required',
+        'description' => 'required',
+        'content' => 'required',
+        'user_id' => 'required|exists:users,id',
+        'type_id' => 'required|exists:topic_type,id',
+        'sort_id' => 'required|exists:topic_sort,id',
+    );
+
     public function sort()
     {
         return $this->hasOne('App\Models\TopicsSort','id','sort_id');
