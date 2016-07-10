8463e87146ee32608b50503430358d2c263b3c42
邱奕玮 <tomhaoye@163.com>
2016-07-10 23:12:38 +0800

    sidebar js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -157,6 +157,47 @@
 <script src="{{ elixir('js/all.js') }}"></script>
 <script src="http://cdn.bootcss.com/nprogress/0.2.0/nprogress.min.js"></script>
 <script src="http://cdn.bootcss.com/slideout/0.1.12/slideout.min.js"></script>
+<script type="text/javascript">
+    (function($) {
+        $.expr[":"].Contains = function(a, i, m) {
+            return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+        };
+        function filterList(header, list) {
+            //@header 头部元素
+            //@list 无需列表
+            //创建一个搜素表单
+            var form = $("<form>").attr({
+                "class":"filterform",
+                action:"#"
+            }), input = $("<input>").attr({
+                "class":"filterinput",
+                type:"text"
+            });
+            $(form).append(input).appendTo(header);
+            $(input).change(function() {
+                var filter = $(this).val();
+                if (filter) {
+                    $matches = $(list).find("a:Contains(" + filter + ")").parent();
+                    $("li", list).not($matches).slideUp();
+                    $matches.slideDown();
+                } else {
+                    $(list).find("li").slideDown();
+                }
+                return false;
+            }).keyup(function() {
+                $(this).change();
+            });
+        }
+        $(function() {
+            filterList($("#form"), $("#demo-list"));
+        });
+    })(jQuery);
+</script>
+<script type="text/javascript">
+
+    jQuery("#jquery-accordion-menu").jqueryAccordionMenu();
+
+</script>
 <script>
     var slideout = new Slideout({
         'panel': document.getElementById('main_panel'),
