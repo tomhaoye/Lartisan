3292287904113b0edce2e82541ebf7a1b3d2fdcb
vagrant <vagrant@homestead>
2017-05-28 10:55:36 +0000

    use redis for session

--- config/database.php
+++ config/database.php
@@ -123,6 +123,13 @@ return [
             'database' => 0,
         ],
 
+        'session' => [
+            'host'     => env('REDIS_HOST', 'localhost'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port'     => env('REDIS_PORT', 6379),
+            'database' => 1,
+        ],
+
     ],
 
 ];
